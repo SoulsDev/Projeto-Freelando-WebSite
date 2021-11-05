4edd6bdbe66e92db52ca8edaf283620be4e1051c
Claudio Hanada <claudiohanada427@gmail.com>
2021-11-05 19:45:59 -0300

    Biblioteca mongodb adicionada com composer

--- src/classes/conexao.php
+++ src/classes/conexao.php
@@ -3,9 +3,9 @@
     try{
         $servidor_bd_sql = "Localhost";
         $usuario_bd_sql = "root";
-        $senha_bd_sql = "";
+        //$senha_bd_sql = "";
         //TODO comentar antes de comitar
-        //$senha_bd_sql = "Cyh941069833*";
+        $senha_bd_sql = "Cyh941069833*";
         $banco = "freelando";
 
         $con = new PDO("mysql:host=$servidor_bd_sql;dbname=$banco", $usuario_bd_sql, $senha_bd_sql);
@@ -18,10 +18,6 @@
         require '../../vendor/autoload.php';
         $mongo_client = new MongoDB\Client("mongodb://localhost:27017");
         $collection = $mongo_client->freelando->postagem;  // seleciona o banco do mongo -> seleciona a coleção do mongo 
-        
-        $result = $collection->insertOne( [ 'name' => 'Hinterland', 'brewery' => 'BrewDog' ] );
-        
-        echo "Inserted with Object ID '{$result->getInsertedId()}'";
 
     }
     catch(Exception $e ){
