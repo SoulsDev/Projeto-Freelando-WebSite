bddcfc4e25be0fd02af7a87e3d7c5792823e747c
Alyson <alyson.subrim2002@gmail.com>
2021-11-11 00:14:46 -0300

    Adiciona pacote do profissional com base na do contratante

---
+++ src/classes/profssional/InsProfissional.php
@@ -0,0 +1,26 @@
+<?php
+
+include_once 'Contratante.php';
+include_once 'mensagemErro.php';
+
+$nome = $_POST['nome'];
+$cpf = $_POST['cpf'];
+$genero = $_POST['genero'];
+$dtNasc = $_POST['data_nascimento'];
+$email = $_POST['email'];
+$numCelular = $_POST['celular'];
+$senha = $_POST['senha'];
+$cep = $_POST['cep'];
+
+$contratante = new Contratante($nome, $email, $senha);
+$consulta = $contratante-> consultaEmail($email);
+
+if($consulta){
+     setMensagemErro("Email já cadastrado");
+     header('Location: ../../../pages/CadastroContratante.php');
+}
+else{
+     $contratante->inserirContratante($contratante->getNome(), $contratante->getEmail(), $contratante->getSenha(), $contratante->getDataRegistro(), $contratante->getDataAlteracao());
+}
+
+
