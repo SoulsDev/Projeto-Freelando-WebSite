38dd2b0018def98d0acb67c7f64c61057a0c6509
Claudio Hanada <claudiohanada427@gmail.com>
2021-11-11 18:15:48 -0300

    Mascara para cpf, utilização do viaCep pelo js, novas formas de validação no primeiro formulário do autonomo

--- scripts/script-cadpro.js
+++ scripts/script-cadpro.js
@@ -1,6 +1,26 @@
 var currentTab = 0; // A guia atual é definida para ser a primeira guia (0)
 showTab(currentTab); // Exibe a guia atual
 
+const email_regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+var cpf_input = document.getElementById('cpf');
+
+// Validação e mascara para input CPF
+cpf_input.addEventListener('keydown', function(event){
+    // Verifica se o que foi digitado não é um numero ou não é digito delete ou backspace
+    if (isNaN(event.key) && (event.keyCode !== 8 && event.keyCode !== 46)){
+        // O caractere digitado não é adicionado ao input
+        event.preventDefault()
+    }
+    // A mascara só deve ser usada quando a tecla não for a tecla delete ou backspace
+    // Caso contrario fica num loop eterno
+    if (event.keyCode !== 8 && event.keyCode !== 46){
+        if(cpf_input.value.length === 3 ||cpf_input.value.length === 7){
+            cpf_input.value = cpf_input.value + '.'
+        }
+        if(cpf_input.value.length === 11)cpf_input.value = cpf_input.value + '-'
+    }
+})
 
 
 function showTab(n) {
@@ -27,7 +47,9 @@ function nextPrev(n) {
     // Esta função descobrirá qual guia exibir
     var x = document.getElementsByClassName("tab");
     // Saia da função se algum campo da guia atual for inválido:
-    if (n == 1 && !validateForm()) return false;
+    if (n == 1 && !validateForm()) {
+        return false;
+    }
     // Ocultar a guia atual:
     x[currentTab].style.display = "none";
     // Aumentar ou diminuir a guia atual em 1:
@@ -50,11 +72,31 @@ function validateForm() {
     // Um loop que verifica cada campo de entrada na guia atual:
     for (i = 0; i < y.length; i++) {
         // Se um campo estiver vazio ...
-        if (y[i].value == "a") {
+        if (y[i].value == "") {
+            // complemento é um campo opcional
+            if (y[i].id !== 'complemento'){
             // adiciona uma classe "inválida" ao campo:
             y[i].className += " invalid";
             // e definir o status válido atual para falso
             valid = false;
+            // validação de cpf devve pedir para ser do tamanho máximo do campo
+            }
+        }
+        if(y[i].id === 'email'){
+            // Caso o email informado não tenha ao menos um '@', '.', dois digitos apos o ponto, e ao menos um char antes do @ ele não 
+            // é um email válido 
+            if(!(email_regex.test(y[i].value.toLowerCase()))){
+                y[i].className += " invalid";
+                valid = false;
+            }
+        }
+        if(y[i].id === 'cpf'){
+            if(y[i].value.length === 14){
+                valid = true;
+            }else{
+                y[i].className += " invalid";
+                valid = false;
+            }
         }
     }
     // Se o status válido for verdadeiro, marque a etapa como concluída e válida:
@@ -76,6 +118,54 @@ function fixStepIndicator(n) {
         x[n + 1].className = x[n + 1].className.replace(" finish", "");
     }
 
-
     x[n].className += " active";
 }
+
+
+function meu_callback(conteudo) {
+    if (!("erro" in conteudo)) {
+        //Atualiza os campos com os valores.
+        document.getElementById('logradouro').value=(conteudo.logradouro);
+        // TODO adicionar o input pra podermos fazer o radar
+        //document.getElementById('bairro').value=(conteudo.bairro);
+        document.getElementById('cidade').value=(conteudo.localidade);
+        document.getElementById('uf').value=(conteudo.uf);
+    } //end if.
+    else {
+        //CEP não Encontrado.
+        limpa_formulário_cep();
+        alert("CEP não encontrado.");
+    }
+}
+
+
+function pesquisacep(valor) {
+
+    //Nova variável "cep" somente com dígitos.
+    var cep = valor.replace(/\D/g, '');
+
+    //Verifica se campo cep possui valor informado.
+    if (cep != "") {
+
+        //Expressão regular para validar o CEP.
+        var validacep = /^[0-9]{8}$/;
+
+        //Valida o formato do CEP.
+        if(validacep.test(cep)) {
+
+            var script = document.createElement('script');
+
+            //Sincroniza com o callback.
+            script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
+
+            //Insere script no documento e carrega o conteúdo.
+            document.body.appendChild(script);
+
+        } //end if.
+        else {
+            //cep é inválido.
+            limpa_formulário_cep();
+            alert("Formato de CEP inválido.");
+        }
+    }
+};
